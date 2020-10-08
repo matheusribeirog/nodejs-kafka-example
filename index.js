0ab34b42963803f78051c3c44fb2e2fe66f09534
Matheus Ribeiro <30762136+matheusribeirog@users.noreply.github.com>
2020-10-08 17:26:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['ec2-3-83-253-41.compute-1.amazonaws.com:9092']
+  brokers: ['colocarobrokerkafka']
 })
 
 const producer = kafka.producer()
