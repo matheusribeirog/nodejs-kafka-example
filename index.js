d7ac50b92088ceffa5518adb19f4f61822831fd6
Matheus Ribeiro <30762136+matheusribeirog@users.noreply.github.com>
2020-10-12 18:26:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ async function produzir(){
     await producer.disconnect()
 }
 
+async function ler(topico){
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+await consumer.connect()
+await consumer.subscribe({ topic: topico, fromBeginning: true })
+
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
+    console.log({
+      value: message.value.toString(),
+    })
+  },
+})
+}
+
 produzir();
